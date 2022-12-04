732f11b70e579964cf79f1d6cdbe37165c66ff0a
xyrilj <78891643+xyrilj@users.noreply.github.com>
2022-12-03 23:24:45 -0500

    Page object v0.1 and sanity test added

---
+++ e2e/page-objects/todo-list-page.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Locator, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
+
+export class ToDoListHomePage {
+
+  private readonly page_path = 'https://todomvc.com/examples/react/#/';
+  private readonly addNew_css = 'input.new-todo';
+  private readonly itemCounter_css = 'footer span.todo-count';
+
+  private readonly homepage: Page;
+  private readonly addNew: Locator;
+  private itemCounter: Locator;
+
+  constructor(page: Page){
+    this.homepage = page;
+    this.addNew = page.locator(this.addNew_css);
+  }
+
+  async goTo() {
+    this.homepage.goto(this.page_path);
+    await expect(this.addNew).toBeVisible();
+  }
+
+  async addNewToDo(task: string) {
+    await this.addNew.focus();
+    await this.addNew.type(task);
+    await this.homepage.keyboard.press('Enter');
+  }
+
+  async getNumberOfTasksLeft() {
+    this.itemCounter = await this.homepage.locator(this.itemCounter_css);
+    return ((await this.itemCounter.innerText()).split(' ')[0]);
+  }
+}
---
+++ e2e/tests/sanity-tests.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { test, expect } from '@playwright/test';
+import { ToDoListHomePage } from '../page-objects/todo-list-page';
+
+test('user can add a to do list item', async ({ page }) => {
+  const todoHomePage = new ToDoListHomePage(page);
+  await todoHomePage.goTo();
+  await todoHomePage.addNewToDo('Test task to do');
+  await expect(todoHomePage.getNumberOfTasksLeft()).toEqual('1');
+});
